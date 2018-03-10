83a32406d2016c4e2ac309ddf75316cea2dd5d77
huskyhoochu <dfg1499@gmail.com>
2018-03-11 01:11:29 +0900

    Create Header & Footer

---
+++ src/components/structure/Bottom.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <p class="text-center text-muted"><em>© 2018. Zamsee all rights reserved.</em></p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  .jumbotron {
+    margin: 70px 0 0 0;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media (max-width: 1680px) {
+    .jumbotron {
+      height: 300px;
+    }
+    .text-center {
+      font-weight: 200;
+      font-size: 1.5rem;
+    }
+  }
+
+  @media (max-width: 414px) {
+    .jumbotron {
+      height: 200px;
+    }
+    .text-center {
+      font-weight: 200;
+      font-size: 1rem;
+    }
+  }
+</style>
