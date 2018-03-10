263448dfe583ce954beb3689bc054ed0052a5963
huskyhoochu <dfg1499@gmail.com>
2018-03-11 06:55:13 +0900

    Rename Index.vue to Home.vue

--- src/components/view/Home.vue
+++ src/components/view/Home.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="container-fluid">
-    <Navbar/>
     <Picture/>
     <Tagline/>
-    <Bottom/>
   </div>
 </template>
 
 <script>
-import Navbar from '../structure/Navbar'
 import Picture from '../contents/Picture'
 import Tagline from '../contents/Tagline'
-import Bottom from '../structure/Bottom'
 
 export default {
   name: 'Index',
   components: {
-    Navbar,
     Picture,
-    Tagline,
-    Bottom
+    Tagline
   }
 }
 </script>
