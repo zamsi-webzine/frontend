100dc98e48f69c040d0e73c949ce04458be105a2
huskyhoochu <dfg1499@gmail.com>
2018-03-11 01:52:49 +0900

    Create Picture & Tagline components

---
+++ src/components/contents/Picture.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="container" id="picture">
+    <div class="row align-items-end">
+      <div class="col-sm">
+
+      </div>
+      <div class="col-sm-10">
+        <div class="text-center">
+          <button class="btn btn-link rounded-circle" v-on:click="randomNum">
+            <img id="popover" data-toggle="popover" data-placement="bottom" title="어디일까?"
+                 data-content="우리 곁에 있었던 골목길, 교차로, 그리고 낙서들."
+                 :src="imagePath+imageNum+extensions"
+                 class="rounded-circle img-fluid" alt="도시">
+          </button>
+        </div>
+      </div>
+      <div class="col-sm">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'picture',
+  data: function () {
+    return {
+      imagePath: 'https://doy0iundk4hjr.cloudfront.net/static/assets/images/cities/landing_0',
+      imageNum: 1,
+      extensions: '.jpg'
+    }
+  },
+  methods: {
+    randomNum: function (event) {
+      let num = Math.floor(Math.random() * 7) + 1
+      this.imageNum = num
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #picture {
+    padding: 50px 0 50px 0;
+  }
+
+  .col-sm-10 {
+    padding: 0 30px 0 30px;
+  }
+
+</style>
---
+++ src/components/contents/Tagline.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container" id="tagline">
+    <article class="container" id="d-day-title">
+      <h1 class="text-center">2018.05</h1>
+    </article>
+    <article class="container" id="description">
+      <h3 class="text-center display-4">잠시,</h3>
+      <h3 class="text-center display-4">들어가도 되겠습니까</h3>
+    </article>
+  </section>
+</template>
+
+<style scoped>
+  @import url(//fonts.googleapis.com/earlyaccess/kopubbatang.css);
+
+  #tagline {
+    padding: 0;
+  }
+
+  #d-day-title > h1 {
+    margin-bottom: 30px;
+    font-weight: 800 !important;
+  }
+
+  #description {
+    font-family: 'KoPub Batang', serif;
+  }
+
+  @media (max-width: 1680px) {
+
+    #description > h3 {
+      font-size: 4rem !important;
+    }
+  }
+
+  @media (max-width: 414px) {
+
+    #description > h3 {
+      font-size: 2rem !important;
+    }
+  }
+</style>
