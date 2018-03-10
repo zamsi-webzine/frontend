6e4b73e50e480736ab50a5b07a71fd270a730c8f
huskyhoochu <dfg1499@gmail.com>
2018-03-11 08:00:09 +0900

    Create login Form

--- src/components/view/Auth.vue
+++ src/components/view/Auth.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="container-fluid">
+  <div class="container">
+    <form>
+      <div class="form-group row justify-content-md-center">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-6">
+          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+        </div>
+      </div>
+      <div class="form-group row justify-content-md-center">
+        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-6">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group row justify-content-md-center">
+        <label class="col-sm-2 col-form-label"></label>
+        <div class="col-sm-6">
+          <button type="submit" class="btn btn-primary">Sign in</button>
+          <button type="button" class="btn btn-link">Forgot Password?</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -10,7 +31,7 @@ export default {
 </script>
 
 <style scoped>
-  .container-fluid {
-    padding: 0;
-  }
+form {
+  margin: 200px 0 200px 0;
+}
 </style>
