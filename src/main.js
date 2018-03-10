022fc863ea2b9602bd320f83a25c4f057508c058
huskyhoochu <dfg1499@gmail.com>
2018-03-11 07:23:55 +0900

    Set up axios & JWT

--- src/main.js
+++ src/main.js
@@ -6,6 +6,80 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import jwtDecode from 'jwt-decode'
+import Vuex from 'vuex'
+
+Vue.user(Vuex)
+Vue.use(VueAxios, axios)
+
+/* eslint-disable no-new */
+new Vuex.Store({
+  state: {
+    jwt: localStorage.getItem('t'),
+    endpoints: {
+      obtainJWT: 'http://localhost:8000/auth/obtain_token',
+      refreshJWT: 'http://localhost:8000/auth/refresh_token'
+    }
+  },
+  mutations: {
+    updateToken (state, newToken) {
+      localStorage.setItem('t', newToken)
+      state.jwt = newToken
+    },
+    removeToken (state) {
+      localStorage.removeItem('t')
+      state.jwt = null
+    }
+  },
+  actions: {
+    obtainToken (email, password) {
+      const payload = {
+        username: email,
+        password: password
+      }
+
+      axios.post(this.state.endpoints.obtainJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    refreshToken () {
+      const payload = {
+        token: this.state.jwt
+      }
+
+      axios.post(this.state.endpoints.refreshJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    inspectToken () {
+      const token = this.state.jwt
+      if (token) {
+        const decoded = jwtDecode(token)
+        const exp = decoded.exp
+        const origIat = decoded.orig_iat
+
+        if ((Date.now() / 1000) - exp < 1800 && (Date.now() / 1000) - origIat < 628200) {
+          this.dispatch('refreshToken')
+        } else if ((Date.now() / 1000) - exp < 1800) {
+
+        } else {
+
+        }
+      }
+    }
+  }
+})
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
