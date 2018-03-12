69834b62b9ea9b778d195b4683c423c686a0c91e
huskyhoochu <dfg1499@gmail.com>
2018-03-12 15:00:48 +0900

    Add jQuery plugin

--- webpack.config.js
+++ webpack.config.js
@@ -111,6 +111,12 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
+    }),
+    new webpack.ProvidePlugin({
+      $:'jquery',
+      jquery:'jquery',
+      'window.jQuery':'jquery',
+      jQuery:'jquery'
     })
   ])
 }
