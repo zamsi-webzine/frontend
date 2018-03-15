ed5779b1246371cd79258f72567f96aa650ac168
huskyhoochu <dfg1499@gmail.com>
2018-03-15 21:33:18 +0900

    Separate modules of store

---
+++ src/store/modules/profile.js
@@ -0,0 +1,38 @@
+import axios from 'axios/index'
+
+import router from '@/router/index'
+import * as types from '../types'
+
+const actions = {
+  [types.GET_DASHBOARD] ({commit, state}) {
+    axios({
+      method: 'get',
+      url: state.endpoints.dashboard + state.pk.toString() + '/',
+      // django를 위해 CSRF 토큰을 헤더에 실어 보낸다
+      headers: {
+        'X-CSRFToken': 'csrfToken',
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT ' + state.jwt
+      },
+      xsrfCookieName: 'XSRF-TOKEN',
+      xsrfHeaderName: 'X-XSRFTOKEN',
+      // 인증도 true 값으로 보낸다
+      credentials: true
+    }).then(() => {
+      router.replace({
+        name: 'Dashboard',
+        params: {nickname: state.nickname}
+      })
+    }).catch((error) => {
+      console.log(error)
+      // 응답으로 날아온 에러 메시지를 displayMessage mutations로 보낸다
+      if (typeof error.response !== 'undefined') {
+        commit('displayMessage', error.response.data.message)
+      }
+    })
+  }
+}
+
+export default {
+  actions
+}
---
+++ src/store/types.js
@@ -0,0 +1,17 @@
+// getters
+export const CHECKOUT_MSG = 'CHECKOUT_MSG'
+
+// actions: auth
+export const OBTAIN_INFO = 'OBTAIN_INFO'
+export const REFRESH_TOKEN = 'REFRESH_TOKEN'
+export const SIGNUP_DATA = 'SIGNUP_DATA'
+export const RESET_PW = 'RESET_PW'
+
+// actions: profile
+export const GET_DASHBOARD = 'GET_DASHBOARD'
+
+// mutations
+export const UPDATE_INFO = 'UPDATE_INFO'
+export const REMOVE_INFO = 'REMOVE_INFO'
+export const CLEAR_MSG = 'CLEAR_MSG'
+export const SET_MSG = 'SET_MSG'
