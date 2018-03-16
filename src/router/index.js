0b9b3e6844ba45342bd734fd9c465b19d1f83f23
huskyhoochu <dfg1499@gmail.com>
2018-03-16 19:27:37 +0900

    Make lazy routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,80 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import Home from '@/components/view/Home'
-import Auth from '@/components/view/Auth'
-import Profile from '@/components/view/Profile'
-import SignIn from '@/components/authentication/SignIn'
-import SignUp from '@/components/authentication/SignUp'
-import Forgot from '@/components/authentication/Forgot'
-import Activation from '@/components/authentication/Activation'
-import MyPosts from '@/components/contents/MyPosts'
-import Settings from '@/components/contents/Settings'
 
-Vue.use(Router)
+const Auth = resolve => {
+  require.ensure(['@/components/view/Auth'], () => {
+    resolve(require('@/components/view/Auth'))
+  })
+}
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '*',
-      redirect: '/'
-    },
-    {
-      path: '/auth',
-      component: Auth,
-      children: [
-        {name: 'SignIn', path: 'signin', component: SignIn},
-        {name: 'SignUp', path: 'signup', component: SignUp},
-        {name: 'Forgot', path: 'forgot-password', component: Forgot},
-        {name: 'Activation', path: 'activation', component: Activation}
-      ]
-    },
-    {
-      path: '/profile',
-      component: Profile,
-      children: [
-        {name: 'MyPosts', path: ':nickname/posts', component: MyPosts, props: true},
-        {name: 'Settings', path: ':nickname/settings', component: Settings, props: true}
-      ],
-      meta: {requiresAuth: true}
-    }
-  ]
-})
+const Profile = resolve => {
+  require.ensure(['@/components/view/Profile'], () => {
+    resolve(require('@/components/view/Profile'))
+  })
+}
+
+const SignIn = resolve => {
+  require.ensure(['@/components/authentication/SignIn'], () => {
+    resolve(require('@/components/authentication/SignIn'))
+  })
+}
+
+const SignUp = resolve => {
+  require.ensure(['@/components/authentication/SignUp'], () => {
+    resolve(require('@/components/authentication/SignUp'))
+  })
+}
+
+const Forgot = resolve => {
+  require.ensure(['@/components/authentication/Forgot'], () => {
+    resolve(require('@/components/authentication/Forgot'))
+  })
+}
+
+const Activation = resolve => {
+  require.ensure(['@/components/authentication/Activation'], () => {
+    resolve(require('@/components/authentication/Activation'))
+  })
+}
+
+const MyPosts = resolve => {
+  require.ensure(['@/components/contents/MyPosts'], () => {
+    resolve(require('@/components/contents/MyPosts'))
+  })
+}
+
+const Settings = resolve => {
+  require.ensure(['@/components/contents/Settings'], () => {
+    resolve(require('@/components/contents/Settings'))
+  })
+}
+
+export const routes = [
+  {
+    path: '',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '*',
+    redirect: '/'
+  },
+  {
+    path: '/auth',
+    component: Auth,
+    children: [
+      {name: 'SignIn', path: 'signin', component: SignIn},
+      {name: 'SignUp', path: 'signup', component: SignUp},
+      {name: 'Forgot', path: 'forgot-password', component: Forgot},
+      {name: 'Activation', path: 'activation', component: Activation}
+    ]
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    children: [
+      {name: 'MyPosts', path: ':nickname/posts', component: MyPosts, props: true},
+      {name: 'Settings', path: ':nickname/settings', component: Settings, props: true}
+    ],
+    meta: {requiresAuth: true}
+  }
+]
