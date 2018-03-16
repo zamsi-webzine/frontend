dd596823ec2c17dbffd85d24ed186694f02e1abb
huskyhoochu <dfg1499@gmail.com>
2018-03-16 10:54:04 +0900

    Separate sidebar modules

--- src/components/view/Dashboard.vue
+++ src/components/view/Dashboard.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container">
-      <p>Hello, {{GET_INFO.nickname}} ({{GET_INFO.email}})</p>
-  </div>
+    <div class="row">
+      <sidebar/>
+      <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"></main>
+    </div>
 </template>
 
 <script>
+import Sidebar from '../contents/Sidebar'
 import * as types from '@/store/types'
 import { mapGetters } from 'vuex'
 export default {
@@ -14,10 +16,18 @@ export default {
     ...mapGetters([
       types.GET_INFO
     ])
+  },
+  components: {
+    Sidebar
   }
 }
 </script>
 
-<style scoped>
-
+<style>
+  header {
+    margin: 0 !important;
+  }
+  footer {
+    margin: 0 !important;
+  }
 </style>
