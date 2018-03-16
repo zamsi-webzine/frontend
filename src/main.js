da10f99bdb4d1d67863717e14dde88aaba0fb405
huskyhoochu <dfg1499@gmail.com>
2018-03-16 19:26:20 +0900

    Exports vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 import axios from 'axios'
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
-import '../static/fontawesome-all.min'
 
-import router from './router/index'
+import { routes } from './router/index'
 import store from './store/index'
 
 Vue.config.productionTip = false
@@ -15,6 +15,14 @@ Vue.config.productionTip = false
 /* Axios Configuration */
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+/* VueRouter */
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 /* eslint-disable no-new */
 new Vue({
