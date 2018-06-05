4ac925092e970bd94b8679d005e6052a02ae6f68
huskyhoochu <dfg1499@gmail.com>
2018-06-05 18:05:20 +0900

    Add date information

--- src/components/contents/client/ClientPostQuery.vue
+++ src/components/contents/client/ClientPostQuery.vue
@@ -3,15 +3,16 @@
     <div class="row m-5">
       <router-link :id="index"
                    :to="{name: 'ClientPost', params: {pk: value.pk}}"
-                   class="card mb-3"
+                   class="card mb-3 hovering text-dark"
                    v-for="(value, index) in getPostList.results"
                    :key="index.id">
         <div class="card-body">
           <h3 class="card-title">{{value.title}}</h3>
           <p class="card-text">
             <span>{{value.author.nickname}}</span>
-            <span>|</span>
+            <br>
             <span>{{callCategory(value.category)}}</span>
+            <span class="text-muted"> ❖ <em>{{dateCreated(value.date_created)}}</em></span>
           </p>
         </div>
       </router-link>
@@ -27,6 +28,11 @@ export default {
     this.fetchData()
   },
   methods: {
+    // 리스트로 들어오는 파이썬 생성 일자를 자바스크립트 생성 일자로 변환
+    dateCreated (payload) {
+      const date = new Date(payload)
+      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    },
     callCategory (payload) {
       const categoryObject = {
         'R': 'Re-View',
@@ -62,7 +68,7 @@ export default {
       this.$store.dispatch('getClientPostList')
       setTimeout(() => {
         this.makeCSS()
-      }, 500)
+      }, 1000)
     }
   },
   computed: {
@@ -80,5 +86,12 @@ export default {
     height: 3rem;
     width: 3rem;
     border-radius: 50%;
+  }
+  .hovering:hover {
+    text-decoration: none;
+  }
+  .card-title {
+    font-family: 'Noto Serif', serif;
+    font-weight: 400;
   }
 </style>
