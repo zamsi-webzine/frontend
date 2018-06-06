ae8d6013c1424d4ed72297648f4711ec5ca000bc
huskyhoochu <dfg1499@gmail.com>
2018-06-06 20:05:16 +0900

    Implement editor profile button

--- src/components/structure/Navbar.vue
+++ src/components/structure/Navbar.vue
@@ -8,8 +8,30 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav ml-auto mr-4">
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+
+        <!--로그인 시 유저 창-->
+        <div v-if="token" class="dropdown mr-2">
+          <a v-if="token" href="#" role="button"
+             id="dropdownMenuLink"
+             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img v-if="getInfo.thumbnail !== 'null'"
+                 class="thumbnail"
+                 :src="getInfo.thumbnail" alt="user-thumbnail">
+            <img v-else
+                 class="img-fluid"
+                 width="100rem"
+                 src="../../assets/icons/user-circle.svg" alt="default-user-thumbnail">
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <router-link tag="a" :to="{name: 'MyPosts'}" class="dropdown-item" href="#">My Posts</router-link>
+            <a class="dropdown-item" @click="signOut">Sign Out</a>
+          </div>
+        </div>
+
+        <!--카테고리 메뉴-->
+        <ul class="navbar-nav mr-4">
           <li class="nav-item">
             <a class="nav-link" href="#"
                data-toggle="collapse" data-target=".navbar-collapse.show">About</a>
@@ -32,6 +54,27 @@
   </header>
 </template>
 
+<script>
+import {mapGetters, mapActions} from 'vuex'
+export default {
+  data () {
+    return {
+      token: localStorage.getItem('token')
+    }
+  },
+  methods: {
+    ...mapActions([
+      'signOut'
+    ])
+  },
+  computed: {
+    ...mapGetters([
+      'getInfo'
+    ])
+  }
+}
+</script>
+
 <style scoped>
   .container-fluid {
     padding: 0;
@@ -48,5 +91,13 @@
     #main-logo {
       width: 120px;
     }
+  }
+
+  /*썸네일 정사각형으로 만들기*/
+  .thumbnail {
+    object-fit: cover;
+    height: 3rem;
+    width: 3rem;
+    border-radius: 50%;
   }
 </style>
